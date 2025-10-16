7bdafa1f1c1c19cf65573f42f64f77e84b863213
Justin Arndt <justinarndt05@gmail.com>
2025-10-15 20:17:53 -0400

    feat(package): Create top-level package API

--- cobra/__init__.py
+++ cobra/__init__.py
@@ -1,1 +1,7 @@
-from .stdlib.array import CobraArray
+# cobra/__init__.py
+
+# Expose the core, user-facing components of the Cobra library
+# at the top-level package namespace.
+
+from .stdlib import CobraArray
+from .compiler import jit
